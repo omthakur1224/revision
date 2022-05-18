0b0124c18d8ff7d4e95f9c591561229e2f7fafe2
omthakur1224 <omthakur1224@gmail.com>
2022-05-18 11:12:12 +0530

    effect of var declaration in for loop

--- javascript/EffectOfVarInFor.js
+++ javascript/EffectOfVarInFor.js
@@ -1,7 +1,7 @@
 var arr = [1,2,3,4,5]
 
 for(var i=0; i<arr.length; i++){
-    console.log('Hello i before',i)
+    // console.log('Hello i before',i)
   setTimeout(()=>console.log(arr[i],i),i*1000)
-  console.log("i after",i)
+  // console.log("i after",i)
 }
