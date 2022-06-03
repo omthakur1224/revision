29e00ddd0b9b627a456bc7ea63716cf3be8fa1eb
omthakur1224 <omthakur1224@gmail.com>
2022-06-03 20:39:31 +0530

    order

--- razor-pay/src/components/Payment.jsx
+++ razor-pay/src/components/Payment.jsx
@@ -29,22 +29,7 @@ function Payment() {
     }
   return (
           <>
-          {/* {
-  "id":"order_DaZlswtdcn9UNV",
-  "entity":"order",
-  "amount":50000,
-  "amount_paid":0,
-  "amount_due":50000,
-  "currency":"INR",
-  "receipt":"Receipt #20",
-  "status":"created",
-  "attempts":0,
-  "notes": {
-    "key1": "value1",
-    "key2": "value2",
-  "created_at":1572502745
-} */}
-           <form action="" onSubmit={handleOrder}>
+             <form action="" onSubmit={handleOrder}>
              <input type="number" id='amt' placeholder='enter amount' onChange={handleChange}/>
              <br />
              <input type="submit" />
